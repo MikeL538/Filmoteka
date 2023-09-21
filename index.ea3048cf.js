3b664352ab91834fcd0abc6f38def9fd052bf3e3
MikeL538 <126614886+MikeL538@users.noreply.github.com>
2023-09-21 17:12:32 +0000

    Deploying to gh-pages from @ MikeL538/Filmoteka@39ae9411c377629922597dfd518cf72073600439 🚀

---
+++ index.ea3048cf.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".details"),t=document.querySelectorAll(".films__list-item > img"),l=document.querySelector(".details__close-button");t.forEach((t=>{t.addEventListener("click",(()=>{e.classList.add("show-element")}))})),l.addEventListener("click",(()=>{e.classList.remove("show-element")}));
+//# sourceMappingURL=index.ea3048cf.js.map
