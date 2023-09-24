3cd658dfc130cde72b4a97717fc1c4043676ce5c
Viache-Slav <slavikfedko@gmail.com>
2023-09-24 20:17:09 +0200

    scrpll

--- src/js/infiniteScroll.js
+++ src/js/infiniteScroll.js
@@ -1,56 +1,62 @@
-import InfiniteScroll from 'infinite-scroll'; // Importuje moduł do obsługi nieskończonego przewijania.
+import InfiniteScroll from 'infinite-scroll';
 import Notiflix from 'notiflix';
 import { fetchQuery } from './fetch';
 import { loadMovies } from './loadMovies';
 
 const moviesContainer = document.querySelector('.films__list');
-let isLoading = false; // Flaga informująca o tym, czy trwa ładowanie danych.
-let pageNumber = 1;
+let isLoading = false;
+let currentPage = 1;
+let totalPages = 1;
 
 async function fetchSearchResults() {
-  console.log('Fetching...'); // Info czy pobierają się filmy
-  if (isLoading) return; // Sprawdza, czy trwa już ładowanie innych danych.
+  console.log('Fetching page:', currentPage);
+  if (isLoading || currentPage > totalPages) return;
 
-  isLoading = true; // Ustawia flagę na true, oznaczając rozpoczęcie ładowania.
+  isLoading = true;
 
   try {
-    const response = await fetchQuery(pageNumber); // Pobiera wyniki wyszukiwania za pomocą funkcji fetchQuery.
-    const movies = response && response.results; // Sprawdza, czy są wyniki wyszukiwania.
+    const response = await fetchQuery(currentPage); 
+    console.log('Request URL:', `search/movie?query=&page=${currentPage}`);
+    const movies = response && response.results;
 
     if (movies) {
-      loadMovies(movies); // Ładuje filmy do kontenera, jeśli są dostępne.
-      pageNumber++;
-      if (movies.length < 20) {
-        Notiflix.Notify.info('No more movies to load.'); // Wyświetla komunikat o braku kolejnych filmów. Coś tu nie działa, console.log też nie łapie.
+      console.log('Loading movies:', movies);
+      loadMovies(movies);
+      currentPage++;
+      totalPages = response.total_pages;
+
+      if (currentPage > totalPages) {
+        Notiflix.Notify.info('Less than 20 movies loaded.');
       }
     } else {
-      Notiflix.Notify.info('No more movies to load.'); // Wyświetla komunikat o braku kolejnych filmów. Coś tu nie działa, console.log też nie łapie.
+      Notiflix.Notify.info('No more movies to load.'); 
     }
   } catch (error) {
-    console.log('Error: ' + error);
+    console.error('Error fetching data:', error);
   }
 
-  isLoading = false; // Ustawia flagę na false, sygnalizując zakończenie ładowania.
+  isLoading = false;
 }
 
 let infScroll = new InfiniteScroll(moviesContainer, {
-  path() {
-    return `search/movie?query=&page=${pageNumber}`; // Ustawia ścieżkę do pobrania kolejnych wyników w nieskończonym przewijaniu.
+  path: function () {
+    return `search/movie?query=&page=${currentPage}`;
   },
-  append: '.films__list-item', // Określa elementy do dodania podczas ładowania kolejnych wyników.
+  append: '.films__list-item',
 });
 
 infScroll.on('load', function () {
-  console.log('InfScrolling'); // Info czy inf scroll działa.
-  fetchSearchResults(); // Wywołuje funkcję pobierania kolejnych wyników.
+  console.log('InfScrolling');
+  fetchSearchResults();
 });
 
 function handleSearchInputChange() {
-  pageNumber = 1; // Resetuje numer strony na 1.
-  moviesContainer.innerHTML = ''; // Czyści kontener filmów.
-
-  fetchSearchResults(); // Wywołuje funkcję pobierania wyników dla nowego zapytania.
+  currentPage = 1;
+  totalPages = 1;
+  moviesContainer.innerHTML = '';
+  fetchSearchResults();
+  console.log('Loaded page:', currentPage);
 }
 
-const searchInput = document.querySelector('.header__nav-input'); // Pobiera pole wprowadzania zapytania.
-searchInput.addEventListener('submit', handleSearchInputChange); // Dodaje nasłuchiwanie na zmiany w polu wprowadzania zapytania.```
+const searchInput = document.querySelector('.header__nav-input');
+searchInput.addEventListener('input', handleSearchInputChange); 
