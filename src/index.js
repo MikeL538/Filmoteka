9160ba4d9a9ade4319571afe8b22fd6f144a3cff
BartoszBaranski90 <bartosz.baranski1990@gmail.com>
2023-09-25 15:42:59 +0200

    adding div in library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './scss/main.scss';
 import { searchMovies, trendingMovies } from './js/handler';
-
+import './js/library';
 window.onload = trendingMovies();
 
 const searchForm = document.querySelector('.header__nav-form');
