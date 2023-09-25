20a16b99c06a7d8c80de0fe388b846ff97b1779f
MikeL538 <126614886+MikeL538@users.noreply.github.com>
2023-09-25 15:52:15 +0200

    Update index.js
    Compatibility with actual index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './scss/main.scss';
-import { searchMovies, trendingMovies } from './js/handler';
+import './js/fetcher';
+import './js/modalDetails';
 import './js/library';
-window.onload = trendingMovies();
-
-const searchForm = document.querySelector('.header__nav-form');
-
-searchForm.addEventListener('submit', searchMovies);
