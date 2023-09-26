ce4abf5e271ebdbb3635f71c7bc6e57f059461bf
BartoszBaranski90 <bartosz.baranski1990@gmail.com>
2023-09-26 17:41:34 +0200

    add cursor

--- src/js/library.js
+++ src/js/library.js
@@ -1,29 +1,37 @@
-// import { detailsMovie } from './detailsMovie';
-// import axios from 'axios';
+import axios from 'axios';
+import './modalDetails';
+import { showDetails } from './modalDetails';
 
-// axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const watchedBtn = document.querySelector('.btn-watched');
+const queueBtn = document.querySelector('.btn-queue');
+const addWatched = document.querySelector('.btn-add-watched');
+const addQueue = document.querySelector('.btn-add-queue');
+const boxWatched = document.querySelector('.watched-box');
+const boxQueue = document.querySelector('.queue-box');
+const imgNoCinema = document.querySelector('#noCinema');
 
-export const watchedBtn = document.querySelector('.btn-watched');
-export const queueBtn = document.querySelector('.btn-queue');
-export const addWatched = document.querySelector('.btn-add-watched');
-export const addQueue = document.querySelector('.btn-add-queue');
-export const boxWatched = document.querySelector('.watched-box');
-export const boxQueue = document.querySelector('.queue-box');
-export const imgNoCinema = document.querySelector('#noCinema');
 console.log(watchedBtn);
 console.log(queueBtn);
+console.log(addWatched);
+console.log(addQueue);
 
 watchedBtn.addEventListener('click', () => {
   boxWatched.classList.remove('is-hiden');
   boxQueue.classList.add('is-hiden');
   imgNoCinema.classList.add('is-hiden');
 });
+
 queueBtn.addEventListener('click', () => {
   boxQueue.classList.remove('is-hiden');
   boxWatched.classList.add('is-hiden');
   imgNoCinema.classList.add('is-hiden');
 });
 
+// addWatched.addEventListener('click', () => {
+//   if (localStorage.getItem('watched') === null) {
+//     localStorage.setItem('watched', '[]');
+//   }
+// });
 
 // export function addingToLibrary() {
 //   if (localStorage.getItem('watched') === null) {
@@ -33,3 +41,17 @@ queueBtn.addEventListener('click', () => {
 //     localStorage.setItem('queue', '[]');
 //   }
 // }
+const options = {
+  method: 'POST',
+  headers: {
+    accept: 'application/json',
+    'content-type': 'application/json',
+    Authorization:
+      "Bearer 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjk0YzNhNWI0MDAwMDg5YWZhMWQ1YTFhZTk4YWIxZCIsInN1YiI6IjY1MGM4MmQzYjViYzIxMDEyY2M5ZmIwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gbA2ivTkeIlFgOsCG0AQU95bbBmYrPkGm6ojq4z3dKo'; // Replace with your API key",
+  },
+};
+
+fetch('https://api.themoviedb.org/3/account/null/watchlist', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
