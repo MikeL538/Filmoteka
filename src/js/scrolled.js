f1e69c06cc77c4f2f38e3f6f32c50258b7210994
MikeL538 <126614886+MikeL538@users.noreply.github.com>
2023-09-28 15:53:05 +0200

    Fixes - Fixed header for scroll so it shrinks but everything displays well, - Changed basic img for modal so it's not random photo but 'not found', - Fixed some stylings for header

--- src/js/scrolled.js
+++ src/js/scrolled.js
@@ -1,24 +1,33 @@
 const header = document.querySelector('.header');
+const headerContainer = document.querySelector('.header__container');
+const headerNav = document.querySelector('.header__nav');
+const headerNavContainer = document.querySelector('.header__nav-container');
 const form = document.querySelector('.header__nav-form');
 const error = document.querySelector('.header__error');
 const input = document.querySelector('.header__nav-input');
 const submit = document.querySelector('.header__nav-input-submit');
 const loupe = document.querySelector('.header__nav-input--loupe');
 
-let isHovered = false; 
-let isScrolling = false; 
+let isHovered = false;
+let isScrolling = false;
 
 window.addEventListener('scroll', () => {
   isScrolling = true;
-  if (window.scrollY > 100) {
+  if (window.scrollY > 50) {
     header.classList.add('scrolled');
+    headerNavContainer.classList.add('scrolled-nav-container');
+    headerContainer.classList.add('scrolled-container');
+    headerNav.classList.add('scrolled-container');
     if (isHovered) {
       showElementsWithDelay();
     }
   } else {
     header.classList.remove('scrolled');
+    headerNavContainer.classList.remove('scrolled-nav-container');
     form.classList.remove('show');
     error.classList.remove('show');
+    headerContainer.classList.remove('scrolled-container');
+    headerNav.classList.remove('scrolled-container');
   }
 });
 
@@ -28,6 +37,12 @@ header.addEventListener('mouseenter', () => {
     setTimeout(() => {
       if (isHovered && !isScrolling) {
         showElementsWithDelay();
+        header.classList.remove('scrolled');
+        headerNavContainer.classList.remove('scrolled-nav-container');
+        form.classList.remove('show');
+        error.classList.remove('show');
+        headerContainer.classList.remove('scrolled-container');
+        headerNav.classList.remove('scrolled-container');
       }
     }, 500);
   }
@@ -37,6 +52,8 @@ header.addEventListener('mouseleave', () => {
   isHovered = false;
   form.classList.remove('show');
   error.classList.remove('show');
+  if (header.classList.contains('scrolled')) {
+  }
 });
 
 function showElementsWithDelay() {
