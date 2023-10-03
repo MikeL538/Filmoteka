4aa0449f35556400c4d961d64631c6b6bd1ab129
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-10-03 15:58:39 +0200

    naprawiona kolejka
    a lot of work

--- src/js/library.js
+++ src/js/library.js
@@ -1,37 +1,53 @@
 import axios from 'axios';
 import { currentLanguage } from './language';
 import { apiKey } from './fetcher';
+import { showLibraryDetails } from './modalDetailsLibrary';
 
-// Funkcja do usuwania filmu z listy
-function removeFromList(listName, movieId) {
-  const list = JSON.parse(localStorage.getItem(listName)) || [];
-  const updatedList = list.filter(id => id !== movieId);
-  localStorage.setItem(listName, JSON.stringify(updatedList));
+//elementu filmu
+function createMovieListItem(movieDetails) {
+  const movieListItem = document.createElement('li');
+  // obsluga zdarzenia kliknięcia do elementu filmu
+  movieListItem.addEventListener('click', () => {
+    showLibraryDetails(movieDetails);
+  });
+
+  return movieListItem;
 }
 
-// szczegóły filmu w modalu
-function showLibraryDetails(event) {
-  // ...
+// wyświetlanie listy filmów
+function displayMovieList(movieList) {
+  const libraryFilmsList = document.querySelector('.library-films__list');
+  libraryFilmsList.innerHTML = '';
 
-  // Usunięcie filmu z kolejki
-  if (event.target.classList.contains('btn-remove-from-queue')) {
-    const movieId = event.target.dataset.id;
-    removeFromList('queue', movieId); // Usuwanie filmu z kolejki
-    event.target.closest('.library-films__list-item').remove(); // Usuwanie filmu z widoku
-    return;
-  }
-
-  // Usunięcie filmu z obejrzanych
-  if (event.target.classList.contains('btn-remove-from-watched')) {
-    const movieId = event.target.dataset.id;
-    removeFromList('watched', movieId); // Usuwanie filmu z obejrzanych
-    event.target.closest('.library-films__list-item').remove(); // Usuwanie filmu z widoku
-    return;
-  }
-
-  // ...
+  movieList.forEach(movieId => {
+    fetchMovieDetails(movieId)
+      .then(movieDetails => {
+        const movieListItem = createMovieListItem(movieDetails);
+        libraryFilmsList.appendChild(movieListItem);
+      })
+      .catch(error => console.error('Error fetching movie details:', error));
+  });
 }
 
+// obsługa "Watched"
+const btnWatched = document.querySelector('.btn-watched');
+if (btnWatched) {
+  btnWatched.addEventListener('click', () => {
+    const watchedList = JSON.parse(localStorage.getItem('watched')) || [];
+    displayMovieList(watchedList);
+  });
+}
+
+// obsługa "Queued"
+const btnQueued = document.querySelector('.btn-queued');
+if (btnQueued) {
+  btnQueued.addEventListener('click', () => {
+    const queueList = JSON.parse(localStorage.getItem('queue')) || [];
+    displayMovieList(queueList);
+  });
+}
+
+// Button watched = watched list
 if (document.querySelector('.library-films__list')) {
   const btnWatched = document.querySelector('.btn-watched');
 
@@ -41,81 +57,7 @@ if (document.querySelector('.library-films__list')) {
 
     const watchedList = JSON.parse(localStorage.getItem('watched')) || [];
 
-    function fetchMovieDetails(movieId, listName) {
-      const movieDetailsUrl = `movie/${movieId}`;
-
-      axios
-        .get(movieDetailsUrl, {
-          params: {
-            api_key: apiKey,
-            language: currentLanguage,
-          },
-        })
-        .then(response => {
-          const movieDetails = response.data;
-          const genreNames = movieDetails.genres.map(genre => genre.name).slice(0, 2).join(', ');
-
-          const movieListItem = document.createElement('li');
-          movieListItem.classList.add('library-films__list-item');
-          movieListItem.dataset.id = movieDetails.id;
-
-          // Dodawanie przycisków "Remove from Queue" i "Remove from Watched"
-          let buttonsHtml = '';
-          if (listName === 'queue') {
-            buttonsHtml = `
-              <button class="btn-remove-from-queue" data-id="${movieDetails.id}">Remove from Queue</button>
-            `;
-          } else if (listName === 'watched') {
-            buttonsHtml = `
-              <button class="btn-remove-from-watched" data-id="${movieDetails.id}">Remove from Watched</button>
-            `;
-          }
-
-          movieListItem.innerHTML = `
-            <img src="${getMovieImagePath(movieDetails.backdrop_path)}" alt="${
-            movieDetails.title
-          }" />
-            <h2>${movieDetails.title}</h2>
-            <p>${genreNames} | <span>${movieDetails.release_date.substring(
-            0,
-            4,
-<<<<<<< HEAD
-          )}</span> <span class="films__list-item--rating">${movieDetails.vote_average}</span></p>
-            ${buttonsHtml}
-=======
-          )}</span> <span class="films__list-item--rating">${Math.round(movieDetails.vote_average * 10) / 10}</span></p>
->>>>>>> 2dcb22e3ca4b395c9b1ad9eeeab47746c2fbfe3b
-          `;
-
-          libraryFilmsList.appendChild(movieListItem);
-
-          movieListItem.addEventListener('click', showLibraryDetails);
-        })
-        .catch(error => console.error('Error fetching movie details:', error));
-    }
-
-    function getMovieImagePath(path) {
-      return path
-        ? `https://image.tmdb.org/t/p/w500${encodeURIComponent(path)}`
-        : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png';
-    }
-
-    watchedList.forEach(movieId => {
-      fetchMovieDetails(movieId, 'watched');
-    });
-  });
-}
-
-if (document.querySelector('.library-films__list')) {
-  const btnQueued = document.querySelector('.btn-queued');
-
-  btnQueued.addEventListener('click', () => {
-    const libraryFilmsList = document.querySelector('.library-films__list');
-    libraryFilmsList.innerHTML = '';
-
-    const queueList = JSON.parse(localStorage.getItem('queue')) || [];
-
-    function fetchMovieDetails(movieId, listName) {
+    function fetchMovieDetails(movieId) {
       const movieDetailsUrl = `movie/${movieId}`;
 
       axios
@@ -132,19 +74,6 @@ if (document.querySelector('.library-films__list')) {
           const movieListItem = document.createElement('li');
           movieListItem.classList.add('library-films__list-item');
           movieListItem.dataset.id = movieDetails.id;
-
-          // Dodawanie przycisków "Remove from Queue" i "Remove from Watched"
-          let buttonsHtml = '';
-          if (listName === 'queue') {
-            buttonsHtml = `
-              <button class="btn-remove-from-queue" data-id="${movieDetails.id}">Remove from Queue</button>
-            `;
-          } else if (listName === 'watched') {
-            buttonsHtml = `
-              <button class="btn-remove-from-watched" data-id="${movieDetails.id}">Remove from Watched</button>
-            `;
-          }
-
           movieListItem.innerHTML = `
             <img src="${getMovieImagePath(movieDetails.backdrop_path)}" alt="${
             movieDetails.title
@@ -154,10 +83,76 @@ if (document.querySelector('.library-films__list')) {
             0,
             4,
           )}</span> <span class="films__list-item--rating">${movieDetails.vote_average}</span></p>
-            ${buttonsHtml}
           `;
 
-          libraryFilmsList.appendChild(movieListItem);
+          // sprawdzam czy film już istnieje na liście
+          const existingMovie = libraryFilmsList.querySelector(`[data-id="${movieDetails.id}"]`);
+          if (!existingMovie) {
+            // jeśli nie, dodaj go
+            libraryFilmsList.appendChild(movieListItem);
+          }
+
+          movieListItem.addEventListener('click', showLibraryDetails);
+        })
+        .catch(error => console.error('Error fetching movie details:', error));
+    }
+
+    function getMovieImagePath(path) {
+      return path
+        ? `https://image.tmdb.org/t/p/w500${encodeURIComponent(path)}`
+        : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png';
+    }
+
+    watchedList.forEach(movieId => {
+      fetchMovieDetails(movieId);
+    });
+  });
+}
+
+// Button queued = queued list
+if (document.querySelector('.library-films__list')) {
+  const btnQueued = document.querySelector('.btn-queued');
+
+  btnQueued.addEventListener('click', () => {
+    const libraryFilmsList = document.querySelector('.library-films__list');
+    libraryFilmsList.innerHTML = '';
+
+    const queueList = JSON.parse(localStorage.getItem('queue')) || [];
+
+    function fetchMovieDetails(movieId) {
+      const movieDetailsUrl = `movie/${movieId}`;
+
+      axios
+        .get(movieDetailsUrl, {
+          params: {
+            api_key: apiKey,
+            language: currentLanguage,
+          },
+        })
+        .then(response => {
+          const movieDetails = response.data;
+          const genreNames = movieDetails.genres.map(genre => genre.name).join(', ');
+
+          const movieListItem = document.createElement('li');
+          movieListItem.classList.add('library-films__list-item');
+          movieListItem.dataset.id = movieDetails.id;
+          movieListItem.innerHTML = `
+            <img src="${getMovieImagePath(movieDetails.backdrop_path)}" alt="${
+            movieDetails.title
+          }" />
+            <h2>${movieDetails.title}</h2>
+            <p>${genreNames} | <span>${movieDetails.release_date.substring(
+            0,
+            4,
+          )}</span> <span class="films__list-item--rating">${movieDetails.vote_average}</span></p>
+          `;
+
+          // sprawdzam czy film już istnieje na liście
+          const existingMovie = libraryFilmsList.querySelector(`[data-id="${movieDetails.id}"]`);
+          if (!existingMovie) {
+            // jeśli nie, dodaj go
+            libraryFilmsList.appendChild(movieListItem);
+          }
 
           movieListItem.addEventListener('click', showLibraryDetails);
         })
@@ -171,7 +166,7 @@ if (document.querySelector('.library-films__list')) {
     }
 
     queueList.forEach(movieId => {
-      fetchMovieDetails(movieId, 'queue');
+      fetchMovieDetails(movieId);
     });
   });
 }
