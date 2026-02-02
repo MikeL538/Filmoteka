e3b1088986f470a318bfaae1f5318803896be552
MikeL538 <mikel538.work@gmail.com>
2026-02-02 21:33:47 +0100

    ref: styles+markup for header-library and JS for scolling header

--- src/js/scrolled.js
+++ src/js/scrolled.js
@@ -2,41 +2,25 @@ const header = document.querySelector('.header');
 const headerForm = document.querySelector('.header__nav-form');
 const headerNav = document.querySelector('.header__nav');
 const headerContainer = document.querySelector('.header__container');
-const libraryButtons = document.querySelector('.library-btn-list');
-const isScrolling = () => window.scrollY > 60;
-// const isScrollingForm = () => window.scrollY >= 50;
+const libraryButtons = document.querySelector('.header--library__btn--container');
+const isScrolling = () => window.scrollY > 70;
 
 window.addEventListener('scroll', () => {
   if (isScrolling()) {
-    header.classList.add('scrolled');
     if (!header.matches(':hover')) {
+      header.classList.add('scrolled');
+
       headerNav.classList.add('nav-height');
       headerContainer.classList.add('scrolled-container');
-    }
-
-    if (libraryButtons && !header.matches(':hover')) {
-      libraryButtons.classList.add('buttons-transform');
+      if (headerForm) headerForm.classList.add('form-transform');
+      if (libraryButtons) libraryButtons.classList.add('buttons-transform');
     }
   } else {
     header.classList.remove('scrolled');
     headerNav.classList.remove('nav-height');
     headerContainer.classList.remove('scrolled-container');
-
-    if (headerForm) {
-      setTimeout(() => {
-        headerForm.classList.remove('form-transform');
-      }, 200);
-    }
-    if (libraryButtons) {
-      setTimeout(() => {
-        libraryButtons.classList.remove('buttons-transform');
-      }, 200);
-    }
-  }
-  if (header.classList.contains('scrolled')) {
-    if (headerForm && !header.matches(':hover')) {
-      headerForm.classList.add('form-transform');
-    }
+    if (headerForm) headerForm.classList.remove('form-transform');
+    if (libraryButtons) libraryButtons.classList.remove('buttons-transform');
   }
 });
 
@@ -47,33 +31,39 @@ header.addEventListener('mouseenter', () => {
     headerContainer.classList.remove('scrolled-container');
 
     if (headerForm) {
-      setTimeout(() => {
-        headerForm.classList.remove('form-transform');
-      }, 200);
+      headerForm.classList.remove('form-transform');
     }
     if (libraryButtons) {
-      setTimeout(() => {
-        libraryButtons.classList.remove('buttons-transform');
-      }, 200);
+      libraryButtons.classList.remove('buttons-transform');
     }
   }
 });
 
 header.addEventListener('mouseleave', () => {
-  if (header.classList.contains('scrolled')) {
+  if (isScrolling()) {
     header.classList.remove('hovered');
+    header.classList.add('scrolled');
+
     headerNav.classList.add('nav-height');
     headerContainer.classList.add('scrolled-container');
 
     if (headerForm) {
-      setTimeout(() => {
-        headerForm.classList.add('form-transform');
-      }, 200);
+      headerForm.classList.add('form-transform');
     }
+
     if (libraryButtons) {
-      setTimeout(() => {
-        libraryButtons.classList.add('buttons-transform');
-      }, 200);
+      libraryButtons.classList.add('buttons-transform');
     }
+
+    // if (headerForm) {
+    //   setTimeout(() => {
+    //     headerForm.classList.add('form-transform');
+    //   }, 200);
+    // }
+    // if (libraryButtons) {
+    //   setTimeout(() => {
+    //     libraryButtons.classList.add('buttons-transform');
+    //   }, 200);
+    // }
   }
 });
